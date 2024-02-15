9861fe63fb767277a92d92b7f0c52f2200b5f437
nyomiamunson <nyomigirl12@gmail.com>
2024-02-15 00:11:56 -0500

    Created a Mario Bros inspired Pong game

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    [SerializeField] float speed = 5f;
+    private Rigidbody2D rb;
+    private AudioSource audioSource;
+    private SpriteRenderer spriteRenderer;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody2D rb; //Declare a RigidBody2d variable
         rb = GetComponent<Rigidbody2D>(); //Get a reference to the RigidBody
+        audioSource = GetComponent<AudioSource>(); //Get a reference to our AudioSource
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get a reference to the SpriteRenderer
 
-        //Set velocity
-        rb.velocity = new Vector2(Random.Range(-200, 200), Random.Range(-200, 200));
+        // Set a random direction while maintaining the speed
+        Vector2 randomDirection = Random.insideUnitCircle.normalized;
+        rb.velocity = randomDirection * speed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource audioSource; //Delcare a AudioSource reference variable
+        audioSource = GetComponent<AudioSource>(); //Get a reference to our AudioSource
+        audioSource.Play();
+
+        // Change the color of the ball upon collision
+        spriteRenderer.color = Random.ColorHSV();
     }
 }
